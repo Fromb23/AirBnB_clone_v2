bfc2201210d849b71af0f78ded0d7d27ebd10c55
Fromb23 <rombo.programming@gmail.com>
2024-07-27 22:00:32 +0300

    flask5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,37 +1,64 @@
 #!/usr/bin/python3
-
+"""
+    Flask Application
+"""
 from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """
+    Return hello
+    """
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """
+    Return hbnb
+    """
     return 'HBNB'
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
+    """
+    Return text
+    """
     text = text.replace('_', ' ')
     return 'Python ' + text
+
 
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text='is cool'):
+    """
+    Return text default
+    """
     text = text.replace('_', ' ')
     return 'Python ' + text
 
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
+    """
+    Return number
+    """
     if isinstance(n, int):
         return str(n) + ' is a number'
 
+
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
+    """
+    Return odd/even
+    """
     return render_template('5-number.html', n=n)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)

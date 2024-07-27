ba917e0cebbcd01a82d75e75c81dcb0eab87238f
Fromb23 <rombo.programming@gmail.com>
2024-07-27 21:47:43 +0300

    flask2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,19 +1,33 @@
 #!/usr/bin/python3
-
+"""
+    Flask application
+"""
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """
+    Return hello
+    """
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """
+    Return hbnb
+    """
     return 'HBNB'
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
+    """
+    Return text
+    """
     my_var = text.replace('_', ' ')
     return 'C ' + my_var
 

91fa66b30b3f6e00675ea1e7de4812c637b7a9b0
Fromb23 <rombo.programming@gmail.com>
2024-07-27 21:50:59 +0300

    flask3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,25 +1,43 @@
 #!/usr/bin/python3
-
+"""
+    Flask application
+"""
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """
+    Return hello hbnb
+    """
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """
+    Return hello hbhb
+    """
     return 'HBNB'
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
+    """
+    Return text
+    """
     text = text.replace('_', ' ')
     return 'C ' + text
+
 
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text='is cool'):
+    """
+    Return text default
+    """
     text = text.replace('_', ' ')
     return 'Python ' + text
 
